979a8d002c2fbd2e5fdfa6abe1bc3c0a042acfb5
zahid hasan <zaihid.hasan@gmail.com>
2025-08-16 00:42:51 +0600

    feat: enhance doctor routes with JWT verification for comment and story management

--- backend/src/routes/doctor.route.js
+++ backend/src/routes/doctor.route.js
@@ -1,11 +1,25 @@
 import express from 'express'
-import { doctorRegister } from '../controllers/doctor.controller.js';
+import { jwtVerify } from '../middlewares/auth.middleware.js';
+import { commentOnStory, deleteComment, doctorLogin, doctorRegister, editComment, getAllDoctors, getDoctorById,getAllStories,  getTodaysSchedule,getDoctorClients } from '../controllers/doctor.controller.js';
 import { doctorFileUpload } from '../middlewares/upload.js';
-// NOTE: jwtVerify and other controllers will be used in later features.
+
 
 const doctorRouter = express.Router()
 
 doctorRouter.post('/register', doctorFileUpload,  doctorRegister)
+doctorRouter.post('/login', doctorLogin )
+doctorRouter.get('/all-doctor', getAllDoctors)
 
-// NOTE: All other routes from this file will be added in later features.
+
+doctorRouter.get('/get-doctor/:doctorId', getDoctorById)
+// comment route
+doctorRouter.post("/comment/:storyId", jwtVerify, commentOnStory);
+doctorRouter.get('/stories', jwtVerify, getAllStories); 
+ doctorRouter.put("/edit-comment/:storyId/:commentId", jwtVerify, editComment);
+doctorRouter.delete("/delete-comment/:storyId/:commentId", jwtVerify, deleteComment);
+
+doctorRouter.get('/schedule/today', jwtVerify, getTodaysSchedule);
+doctorRouter.get('/my-clients', jwtVerify, getDoctorClients);
+// Correct route
+doctorRouter.post("/stories/:storyId/comment", jwtVerify, commentOnStory);
 export default doctorRouter;
